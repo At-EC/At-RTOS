dc6945e3339bab8a78b2e7a6e20f481045113abf
Riven Zheng <zhengheiot@gmail.com>
2024-01-21 21:50:13 +0800

    The FW version number only can be changed after the commit changing.

--- kernal/include/atos_version.h
+++ kernal/include/atos_version.h
@@ -1,10 +1,10 @@
 ﻿/**
- * Copyright (c) Riven Zheng (zhengheiot@gmail.com).
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
- 
+* Copyright (c) Riven Zheng (zhengheiot@gmail.com).
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
 #ifndef _ATOS_VERSION_H_
 #define _ATOS_VERSION_H_
 
@@ -12,7 +12,9 @@
 extern "C" {
 #endif
 
-#define ATOS_BUILD_TIME                             "2024-01-21,09:01"
+#define ATOS_BUILD_TIME                             "2024-01-21,21:44"
+
+#define ATOS_HEAD_COMMIT_ID                         "255ac3c16ac993f95ea978c7efb4fefea19e8c58"
 
 #define ATOS_VERSION_STRING                         "0.0.40.0"
 #define ATOS_VERSION_PRODUCTION_RELEASE_NUMBER      (0u)
--- script/auto-version.ps1
+++ script/auto-version.ps1
@@ -16,6 +16,8 @@ extern "C" {
 
 #define ATOS_BUILD_TIME                             "2024-01-01,01:01"
 
+#define ATOS_HEAD_COMMIT_ID                         "255ac3c16ac993f95ea978c7efb4fefea19e8c58"
+
 #define ATOS_VERSION_STRING                         "0.0.0.0"
 #define ATOS_VERSION_PRODUCTION_RELEASE_NUMBER      (0u)
 #define ATOS_VERSION_OFFICIAL_RELEASE_NUMBER        (0u)
@@ -54,10 +56,11 @@ $MatchNumber = '[0-9]+'
 $MatchChanges = '#define ATOS_VERSION_CHANGES_NUMBER                 \([0-9]+u\)'
 $Range = ($ReadContent | Select-String -Pattern $MatchChanges -AllMatches).Matches.Value
 
+$MatchCommitId = '[0123456789abcdef]{40}'
+$ReadCommitId = ($ReadContent | Select-String -Pattern $MatchCommitId -AllMatches).Matches.Value
+
 $Changes = ($Range | Select-String -Pattern $MatchNumber -AllMatches).Matches.Value
 $Changes = [int]$Changes
-$Changes++
-$Changes = [string]$Changes
 
 $MatchProduction = '#define ATOS_VERSION_PRODUCTION_RELEASE_NUMBER      \([0-9]+u\)'
 $Range = ($ReadContent | Select-String -Pattern $MatchProduction -AllMatches).Matches.Value
@@ -71,21 +74,32 @@ $MatchProduction = '#define ATOS_VERSION_CATEGORIES_NUMBER              \([0-9]+
 $Range = ($ReadContent | Select-String -Pattern $MatchProduction -AllMatches).Matches.Value
 $Categories = ($Range | Select-String -Pattern $MatchNumber -AllMatches).Matches.Value
 
-$Generate = $Production + "." + $Official + "." + $Changes + "." + $Categories
+$CommitId = invoke-expression -command "git rev-parse HEAD"
 
+if ( $ReadCommitId -notmatch  $CommitId )
+{
+	$Changes++
+}
+
+$Changes = [string]$Changes
+$Generate = $Production + "." + $Official + "." + $Changes + "." + $Categories
 Write-Host $Generate
 
+if ( $ReadCommitId -match $CommitId )
+{
+	Exit
+}
 $Generate = '"' + $Generate + '"'
 
 $FileContent | Out-File -FilePath $TargetFile -Force -Encoding utf8
 
 $Output = '/**
- * Copyright (c) Riven Zheng (zhengheiot@gmail.com).
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
- 
+* Copyright (c) Riven Zheng (zhengheiot@gmail.com).
+*
+* This source code is licensed under the MIT license found in the
+* LICENSE file in the root directory of this source tree.
+*/
+
 #ifndef _ATOS_VERSION_H_
 #define _ATOS_VERSION_H_
 
@@ -99,6 +113,10 @@ $timestamp = Get-Date -Format "yyyy-MM-dd,HH:mm"
 $Output += '#define ATOS_BUILD_TIME                             "2024-01-01,01:01"
 
 ' -replace '\d{4}-\d{2}-\d{2},\d{2}:\d{2}', $timestamp
+
+$Output += '#define ATOS_HEAD_COMMIT_ID                         "255ac3c16ac993f95ea978c7efb4fefea19e8c58"
+
+' -replace '[0123456789abcdef]{40}', $CommitId
 
 $Output += '#define ATOS_VERSION_STRING                         '
 $Output += $Generate + '
@@ -134,3 +152,4 @@ $Output += '
 #endif'
 
 $Output | Out-File -FilePath $TargetFile -Force -Encoding utf8
+
