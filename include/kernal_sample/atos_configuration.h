690ae2c5cfd48a82fe37d5256befb66dd6272b55
Riven Zheng <zhengheiot@gmail.com>
2024-02-09 20:10:49 +0800

    Add the template configuration define.

--- include/kernal_sample/atos_configuration.h
+++ include/kernal_sample/atos_configuration.h
@@ -13,7 +13,73 @@ extern "C" {
 #endif
 
 #define KERNAL_SAMPLE
- 
+
+/**
+ * If you are use ARM Cortex M seiral architecture and use the system tick as the kernal timer.
+ * In most cases, PORTAL_SYSTEM_CORE_CLOCK_MHZ must be set to the frequency of the clock
+ * that drives the peripheral used to generate the kernels periodic tick interrupt.
+ * The default value is set to 120mhz. Your application will certainly need a different value so set this correctly.
+ * This is very often, but not always, equal to the main system clock frequency.
+ **/
+#define PORTAL_SYSTEM_CORE_CLOCK_MHZ              (120u)
+
+/**
+ * If you are use ARM Cortex M seiral architecture and use the system tick as the kernal timer.
+ * The kernals periodic tick interrupt scheduler needs a minimum time to handle the kernal time function,
+ * The defaule value is set ot 50us when the frequency is 120mhz. Your application will certainly need a different value so set this correctly.
+ * This is very often, but not always, according to the main system clock frequency.
+ **/
+#define PORTAL_SYSTEM_CLOCK_INTERVAL_MIN_US       (50u)
+
+/**
+ * This symbol defined the thread instance number that your application is using.
+ * The defaule value is set to 3. Your application will certainly need a different value so set this correctly.
+ * This is very often, but not always, according to the actual thread instance number that you created.
+ **/
+#define THREAD_INSTANCE_SUPPORTED_NUMBER          (3u)
+
+/**
+ * This symbol defined the semaphore instance number that your application is using.
+ * The defaule value is set to 3. Your application will certainly need a different value so set this correctly.
+ * This is very often, but not always, according to the actual semaphore instance number that you created.
+ **/
+#define SEMAPHORE_INSTANCE_SUPPORTED_NUMBER       (3u)
+
+/**
+ * This symbol defined the event instance number that your application is using.
+ * The defaule value is set to 3. Your application will certainly need a different value so set this correctly.
+ * This is very often, but not always, according to the actual event instance number that you created.
+ **/
+#define EVENT_INSTANCE_SUPPORTED_NUMBER           (3u)
+
+/**
+ * This symbol defined the mutex instance number that your application is using.
+ * The defaule value is set to 3. Your application will certainly need a different value so set this correctly.
+ * This is very often, but not always, according to the actual mutex instance number that you created.
+ **/
+#define MUTEX_INSTANCE_SUPPORTED_NUMBER           (3u)
+
+/**
+ * This symbol defined the queue instance number that your application is using.
+ * The defaule value is set to 5. Your application will certainly need a different value so set this correctly.
+ * This is very often, but not always, according to the actual queue instance number that you created.
+ **/
+#define QUEUE_INSTANCE_SUPPORTED_NUMBER           (3u)
+
+/**
+ * This symbol defined the timer instance number that your application is using.
+ * The defaule value is set to 5. Your application will certainly need a different value so set this correctly.
+ * This is very often, but not always, according to the actual timer instance number that you created.
+ **/
+#define TIMER_INSTANCE_SUPPORTED_NUMBER           (3u)
+
+/**
+ * This symbol defined your thread running mode, if the thread runs at the privileged mode.
+ * The defaule value is set to 0. Your application will certainly need a different value so set this correctly.
+ * This is very often, but not always, according to the security level that you want.
+ **/
+#define THREAD_PSP_WITH_PRIVILEGED                (0u)
+
 #ifdef __cplusplus
 }
 #endif
