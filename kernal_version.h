179c6827d34e76fca225897333fb74e86b6918e8
Riven Zheng <zhengheiot@gmail.com>
2024-02-14 14:39:42 +0800

    Update the local build version.

--- kernal_version.h
+++ kernal_version.h
@@ -12,11 +12,11 @@
 extern "C" {
 #endif
 
-#define ATOS_BUILD_TIME                             "2024-02-14,14:15"
-#define ATOS_COMMIT_HEAD_ID                         "1ef4594d921f51e94117340afd6108816c1af747"
+#define ATOS_BUILD_TIME                             "2024-02-14,14:39"
+#define ATOS_COMMIT_HEAD_ID                         "76507f28dc0b3c36c8a6be115f10204351fc391e"
 #define ATOS_VERSION_MAJOR_NUMBER                   (0u)
 #define ATOS_VERSION_MINOR_NUMBER                   (3u)
-#define ATOS_VERSION_PATCH_NUMBER                   (1u)
+#define ATOS_VERSION_PATCH_NUMBER                   (2u)
 
 #define ATOS_VERSION_MAJOR_NUMBER_MASK              (0x03FFu)
 #define ATOS_VERSION_MAJOR_NUMBER_POS               (22u)
