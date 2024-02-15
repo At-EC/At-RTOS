b01ae1f9039e79a11ca7254b77315aac14346e34
At-EC <At-EC@users.noreply.github.com>
2024-02-15 13:02:59 +0000

    [v1.0.0]: At-RTOS release the first production firmware.

--- kernal_version.h
+++ kernal_version.h
@@ -12,11 +12,11 @@
 extern "C" {
 #endif
 
-#define ATOS_BUILD_TIME                             "2024-02-15,20:56"
-#define ATOS_COMMIT_HEAD_ID                         "5c95f1a3f050128d4a5c180e8f9b93f6737421f4"
-#define ATOS_VERSION_MAJOR_NUMBER                   (0u)
-#define ATOS_VERSION_MINOR_NUMBER                   (3u)
-#define ATOS_VERSION_PATCH_NUMBER                   (7u)
+#define ATOS_BUILD_TIME                             "2024-02-15,13:02"
+#define ATOS_COMMIT_HEAD_ID                         "e08d97346e4ace8af3e22b683604fb0ed0ebd2e9"
+#define ATOS_VERSION_MAJOR_NUMBER                   (1u)
+#define ATOS_VERSION_MINOR_NUMBER                   (0u)
+#define ATOS_VERSION_PATCH_NUMBER                   (0u)
 
 #define ATOS_VERSION_MAJOR_NUMBER_MASK              (0x03FFu)
 #define ATOS_VERSION_MAJOR_NUMBER_POS               (22u)
