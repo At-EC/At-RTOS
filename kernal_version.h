9f0c4b8b830f51ce1b429fbe6cba32e68fc41200
At-EC <At-EC@users.noreply.github.com>
2024-02-25 09:05:56 +0000

    [v1.2.0]: At-RTOS release new firmware.

--- kernal_version.h
+++ kernal_version.h
@@ -12,11 +12,11 @@
 extern "C" {
 #endif
 
-#define ATOS_BUILD_TIME                             "2024-02-25,16:42"
-#define ATOS_COMMIT_HEAD_ID                         "af8f7c7537d41ee9ca5a350f091b2a49f84b82ba"
+#define ATOS_BUILD_TIME                             "2024-02-25,09:05"
+#define ATOS_COMMIT_HEAD_ID                         "66a95ee26a11bf249818d3ca1cdf5e3720cd21d6"
 #define ATOS_VERSION_MAJOR_NUMBER                   (1u)
-#define ATOS_VERSION_MINOR_NUMBER                   (1u)
-#define ATOS_VERSION_PATCH_NUMBER                   (10u)
+#define ATOS_VERSION_MINOR_NUMBER                   (2u)
+#define ATOS_VERSION_PATCH_NUMBER                   (00u)
 
 #define ATOS_VERSION_MAJOR_NUMBER_MASK              (0x03FFu)
 #define ATOS_VERSION_MAJOR_NUMBER_POS               (22u)
